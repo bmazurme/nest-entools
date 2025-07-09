7cfb074d070adb3289f9b54caca4adf5da7ce44f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-09 13:33:17 +0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
+import { swaggerConfig } from './common/configs/swagger.config';
+import { loggerConfig } from './common/configs/logger.config';
+import { configureCors } from './common/configs/cors.config';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, loggerConfig);
+  configureCors(app);
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
