289cf7d4bd5c53b9b07ca5a2db59e42c941643b8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-09 13:38:05 +0300

    fix: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { DocumentsModule } from './documents/documents.module';
+import { BlocksModule } from './blocks/blocks.module';
+import { DocumentTypesModule } from './document-types/document-types.module';
+import { RainRunoffItemsModule } from './rain-runoff-items/rain-runoff-items.module';
+import { RolesModule } from './roles/roles.module';
+import { UserRolesModule } from './user-roles/user-roles.module';
+
+import { TypeOrmModuleConfig } from './common/configs/type-orm.config';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, DocumentsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModuleConfig,
+    UsersModule,
+    ProjectsModule,
+    DocumentsModule,
+    BlocksModule,
+    DocumentTypesModule,
+    RainRunoffItemsModule,
+    RolesModule,
+    UserRolesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
