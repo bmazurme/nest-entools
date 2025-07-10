89ce1029d1c3441123a35274245ec2f3c6190e84
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-10 14:03:20 +0300

    fix: project.entity - user-link

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,28 @@
-export class Project {}
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+
+import { BaseEntity } from '../../common/entities/base.entity';
+import { Document } from '../../documents/entities/document.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Project extends BaseEntity {
+  @Column({ unique: true, nullable: false })
+  name: string;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @Column({ nullable: false })
+  address: string;
+
+  @OneToMany(() => Document, (document) => document.project)
+  documents: Document[];
+
+  // Связь с создателем проекта
+  @ManyToOne(() => User, (user) => user.projects, {
+    nullable: false, // Создатель обязателен
+    onDelete: 'CASCADE', // При удалении пользователя удаляем и проекты
+  })
+  @JoinColumn({ name: 'creator_id' }) // Название колонки в БД
+  creator: User;
+}
