b03a8d51f4b70992dd275a5135f6f54e758f329c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-10 18:32:10 +0300

    fix: user.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -53,8 +53,8 @@ export class User extends BaseEntity {
   @ManyToMany(() => Role, (role) => role.users)
   @JoinTable({
     name: 'user_roles',
-    joinColumn: { name: 'user_id', referencedColumnName: 'id' },
-    inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    joinColumn: { name: 'userId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'roleId', referencedColumnName: 'id' },
   })
   roles: Role[];
 
