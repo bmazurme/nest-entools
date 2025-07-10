79fbf402970986aa007fa64ff5033afbc576d8ca
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-10 18:35:04 +0300

    fix: rain-runoff-items module

--- src/rain-runoff-items/entities/rain-runoff-item.entity.ts
+++ src/rain-runoff-items/entities/rain-runoff-item.entity.ts
@@ -1,1 +1,24 @@
-export class RainRunoffItem {}
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+import { BaseEntity } from '../../common/entities/base.entity';
+import { Block } from '../../blocks/entities/block.entity';
+
+@Entity()
+export class RainRunoffItem extends BaseEntity {
+  @Column({
+    nullable: false,
+    length: 100,
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+  })
+  index: number;
+
+  @ManyToOne(() => Block, (block) => block.items, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'blockId' })
+  block: Block;
+}
