bf7af9c52b4c577656cc6442c13350dd7647841e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-14 22:15:16 +0300

    fix: UpdateRainRunoffItemDto

--- src/rain-runoff-items/dto/update-rain-runoff-item.dto.ts
+++ src/rain-runoff-items/dto/update-rain-runoff-item.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateRainRunoffItemDto } from './create-rain-runoff-item.dto';
 
-export class UpdateRainRunoffItemDto extends PartialType(CreateRainRunoffItemDto) {}
+export class UpdateRainRunoffItemDto extends PartialType(
+  CreateRainRunoffItemDto,
+) {
+  id: number;
+  index: number;
+  column: string;
+}
