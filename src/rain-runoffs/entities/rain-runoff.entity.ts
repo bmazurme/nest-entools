80c7a8d90a8ddcc51f94a9a1c7e47f30bb56e302
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-16 16:37:47 +0300

    fix: rain-runoff.entity

--- src/rain-runoffs/entities/rain-runoff.entity.ts
+++ src/rain-runoffs/entities/rain-runoff.entity.ts
@@ -13,13 +13,115 @@ export class RainRunoff extends BaseEntity {
 
   @Column({
     nullable: false,
+    default: 0,
+    type: 'float',
   })
   roof: number;
 
   @Column({
     nullable: false,
+    default: 0,
+    type: 'float',
   })
   pavements: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  tracks: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  ground: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  cobblestone: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  stone: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  lawns: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  place: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  intensity: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  condition: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  timeInit: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  lengthPipe: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  lengthTray: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  velocityPipe: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  velocityTray: number;
+
+  @Column({
+    nullable: false,
+    default: 0,
+    type: 'float',
+  })
+  flow: number;
 
   @OneToOne(() => Item, (item) => item.rainRunoff)
   @JoinColumn()
