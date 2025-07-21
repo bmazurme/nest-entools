c7bfedd1c9d631724cbdcc2bb58572c8a7506c6a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-21 20:23:38 +0300

    fix: crate-block.dto

--- src/blocks/dto/create-block.dto.ts
+++ src/blocks/dto/create-block.dto.ts
@@ -1,1 +1,27 @@
-export class CreateBlockDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBlockDto {
+  @ApiProperty({
+    description: 'Название блока',
+    example: 'Основной блок',
+    required: true,
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'Индекс позиции блока',
+    example: 1,
+    minimum: 0,
+    required: true,
+  })
+  index: number;
+
+  @ApiProperty({
+    description: 'Документ, к которому относится блок',
+    type: Object,
+    required: true,
+  })
+  document: {
+    id: number;
+  };
+}
