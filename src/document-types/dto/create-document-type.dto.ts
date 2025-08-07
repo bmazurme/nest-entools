05d29ae4498f4dea51ae94febafa3b178daae51c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-07 19:45:40 +0300

    fix: document-types

--- src/document-types/dto/create-document-type.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateDocumentTypeDto {
-  constructor({ name, description }: { name: string; description: string }) {
-    this.name = name;
-    this.description = description;
-  }
-
-  name: string;
-  description: string;
-}
