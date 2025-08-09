959ac333589186cc928ed8207d7f4df880d52d0a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-09 09:09:04 +0300

    fix: create-rain-runoff.dto

--- src/rain-runoffs/dto/create-rain-runoff.dto.ts
+++ src/rain-runoffs/dto/create-rain-runoff.dto.ts
@@ -1,1 +1,21 @@
-export class CreateRainRunoffDto {}
+export class CreateRainRunoffDto {
+  id: number;
+  roof: number;
+  pavements: number;
+  tracks: number;
+  ground: number;
+  cobblestone: number;
+  stone: number;
+  lawns: number;
+  place: number;
+  intensity: number;
+  condition: number;
+  timeInit: number;
+  lengthPipe: number;
+  lengthTray: number;
+  velocityPipe: number;
+  velocityTray: number;
+  flow: number;
+  zone: number;
+  type: number;
+}
