a5162e57341719c29f0d46309c5c0c37e4d517ad
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-09 20:31:15 +0300

    feat: get-flow calc util

---
+++ src/calc/get-flow.ts
@@ -0,0 +1,9 @@
+export default function getFlow(
+  zMid: number,
+  a: number,
+  areaSumm: number,
+  timeSumm: number,
+  n: number,
+) {
+  return (zMid * a ** 1.2 * areaSumm) / timeSumm ** (1.2 * n - 0.1);
+}
