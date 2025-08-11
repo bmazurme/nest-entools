f1bcc420584614386ce008deac7e5b6410561671
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-11 19:40:11 +0300

    feat: get-z-mid

---
+++ src/calc/get-z-mid.ts
@@ -0,0 +1,33 @@
+import { KOEF } from './data/constants';
+import { TypeRainArea } from './rain-flow/rain-flow.type';
+
+/**
+ * среднее значение коэффициента покрова, характеризующего поверхность бассейна стока
+ */
+export default function getZMid(
+  area: TypeRainArea,
+  areaSumm: number,
+  z: number,
+) {
+  const {
+    roof = 0,
+    pavements = 0,
+    tracks = 0,
+    ground = 0,
+    cobblestone = 0,
+    stone = 0,
+    lawns = 0,
+  } = area;
+  const { PAVEMENTS, TRACKS, GROUND, COBBLESTONE, STONE, LAWNS } = KOEF;
+
+  return (
+    (roof * z +
+      pavements * PAVEMENTS +
+      tracks * TRACKS +
+      ground * GROUND +
+      cobblestone * COBBLESTONE +
+      stone * STONE +
+      lawns * LAWNS) /
+    areaSumm
+  );
+}
