bba73c217238194731ca14dce94965a677e5e106
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-13 18:11:15 +0300

    fix: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { DocumentsModule } from './documents/documents.module';
 import { BlocksModule } from './blocks/blocks.module';
-import { DocumentTypesModule } from './document-types/document-types.module';
+import { TypesModule } from './types/types.module';
 import { ItemsModule } from './items/items.module';
 import { RolesModule } from './roles/roles.module';
 import { OAuthModule } from './oauth/oauth.module';
+import { RainRunoffsModule } from './rain-runoffs/rain-runoffs.module';
+import { RainRoofsModule } from './rain-roofs/rain-roofs.module';
 
 import { TypeOrmModuleConfig } from './common/configs/type-orm.config';
-import { RainRunoffsModule } from './rain-runoffs/rain-runoffs.module';
 
 @Module({
   imports: [
@@ -23,11 +25,12 @@ import { RainRunoffsModule } from './rain-runoffs/rain-runoffs.module';
     ProjectsModule,
     DocumentsModule,
     BlocksModule,
-    DocumentTypesModule,
+    TypesModule,
     ItemsModule,
     RolesModule,
     OAuthModule,
     RainRunoffsModule,
+    RainRoofsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
